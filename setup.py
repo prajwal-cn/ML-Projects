fb90a491fe67cc8a2075738e2ad4406d09a980d8
prajwal-cn <cnprajwal.official@gmail.com>
2023-03-05 16:41:37 +0530

    The second commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements.
+    '''
+    requirements = []
+    with open (file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+
+setup(
+name='mlprojects',
+version='0.0.1',
+author='prajwal',
+author_email='cnprajwal.official@gmail.com',                     
+packages=find_packages(),
+install_requires= get_requirements('requirements.txt')
+)
+
